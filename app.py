51725719a8b2d1e7f519253eee5071cb717b8af2
rushikeshlokhande1 <rushikeshlokhande982@gmail.com>
2025-09-27 12:19:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -15,5 +15,6 @@ def resume():
 def certificates(filename):
     return send_from_directory('static/certificates', filename)
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))  # use PORT from environment
+    app.run(host="0.0.0.0", port=port)
